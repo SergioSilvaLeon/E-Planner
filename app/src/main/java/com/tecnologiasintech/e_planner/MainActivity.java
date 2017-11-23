b830bfb9ef4ef27360b9e83f05849a818666936f
Lars Kappert <=>
2017-11-23 02:44:38 -0700

    Login, Reset password, Register

--- app/src/main/java/com/tecnologiasintech/e_planner/MainActivity.java
+++ app/src/main/java/com/tecnologiasintech/e_planner/MainActivity.java
@@ -1,7 +1,12 @@
 package com.tecnologiasintech.e_planner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_sign_out){
+            userSignOut();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void userSignOut() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
 }
