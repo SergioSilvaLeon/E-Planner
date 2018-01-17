00e2eefc6a6015283c3c81053ec346f564d5b5b4
Sergio Silva <ssilva@nearsoft.com>
2018-01-17 03:03:05 -0700

    Date Module Create Event

--- app/src/main/java/com/tecnologiasintech/e_planner/activity/CreateEventActivity.java
+++ app/src/main/java/com/tecnologiasintech/e_planner/activity/CreateEventActivity.java
@@ -1,28 +1,45 @@
 package com.tecnologiasintech.e_planner.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.tecnologiasintech.e_planner.R;
 import com.tecnologiasintech.e_planner.model.Event;
 
-import org.w3c.dom.Text;
 
-public class CreateEventActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CreateEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("EPlanner/Event");
 
     EditText editTextName,editTextDate,editTextDescription,editTextHost;
     CheckBox mCheckBoxPopular;
+    String eventDateSelected;
+    LinearLayout viewDate;
+    Spinner spinner;
+    boolean specific = true;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +52,32 @@ public class CreateEventActivity extends AppCompatActivity {
         editTextDate = (EditText) findViewById(R.id.editText_date);
         editTextDescription = (EditText) findViewById(R.id.editText_description);
         editTextHost = (EditText) findViewById(R.id.editText_host);
-        mCheckBoxPopular = (CheckBox) findViewById(R.id.checkBoxPopular  );
+        mCheckBoxPopular = (CheckBox) findViewById(R.id.checkBoxPopular);
+
+        // SetUp Views
+        viewDate  = (LinearLayout) findViewById(R.id.view_date);
+
+
+
+
+        spinner  = (Spinner) findViewById(R.id.spinnerFechaEvento);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.evento_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                eventDateSelected = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner.setVisibility(View.GONE);
 
         Button buttonCreateEvent = (Button) findViewById(R.id.btn_create_Event);
 
@@ -54,7 +96,6 @@ public class CreateEventActivity extends AppCompatActivity {
 
         // upload to firebase
         String name = editTextName.getText().toString();
-        String date = editTextDate.getText().toString();
         String description = editTextDescription.getText().toString();
         String host = editTextHost.getText().toString();
         boolean isPopular = mCheckBoxPopular.isChecked();
@@ -106,10 +147,72 @@ public class CreateEventActivity extends AppCompatActivity {
         // get key
         String key = ref.push().getKey();
 
+        if (!specific){
+            date = eventDateSelected;
+        }
+
         Event event = new Event(name, date, description, isPopular, host, key);
         ref.child(key).setValue(event);
 
 
         finish();
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar cal = new GregorianCalendar(year,month, dayOfMonth);
+        setDate(cal);
+    }
+
+    private void setDate(final Calendar calendar){
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        ((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(calendar.getTime()));
+        date = dateFormat.format(calendar.getTime());
+    }
+
+    public  void datePicker(View view){
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.show(getSupportFragmentManager(), "date");
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_date:
+                if (checked)
+                    // Hide View
+                    spinner.setVisibility(View.GONE);
+                    viewDate.setVisibility(View.VISIBLE);
+                    specific = true;
+                    break;
+            case R.id.radio_general:
+                if (checked)
+                    // Ninjas rule
+                    spinner.setVisibility(View.VISIBLE);
+                    viewDate.setVisibility(View.GONE);
+                    specific = false;
+                    break;
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+            getActivity(), year,month,day);
+        }
+    }
+
+
 }
