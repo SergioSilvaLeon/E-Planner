9d43ea83d69b8e99a6b148f7bb11fb6d9652cbd3
Sergio Silva <ssilva@nearsoft.com>
2018-01-17 01:14:54 -0700

    Update Student Input Validation and Spinner pre-selection

--- app/src/main/java/com/tecnologiasintech/e_planner/activity/UpdateStudentActivity.java
+++ app/src/main/java/com/tecnologiasintech/e_planner/activity/UpdateStudentActivity.java
@@ -3,9 +3,13 @@ package com.tecnologiasintech.e_planner.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,8 +21,16 @@ public class UpdateStudentActivity extends AppCompatActivity {
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("EPlanner/Student");
 
-    EditText editTextName,editTextEmail,editTextSchool,editTextTShirtSize,editTextTechnology,
-            editTextOrginization,editTextSemester;
+    EditText editTextName,editTextEmail,editTextSchool,editTextTechnology,editTextOrginization;
+
+    Spinner spinnerEditTShirtSize;
+    Spinner spinnerEditSemester;
+
+    private String tShirtSizeSelected, semesterSelected;
+
+    String[] semesterArray;
+    String[] tShirtArray;
+
 
     String key;
 
@@ -27,41 +39,122 @@ public class UpdateStudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_student);
 
+        semesterArray = getResources().getStringArray(R.array.semester_array);
+        tShirtArray = getResources().getStringArray(R.array.tshirt_array);
+
+
+
         editTextName = (EditText) findViewById(R.id.editText_name);
         editTextEmail = (EditText) findViewById(R.id.editText_email);
         editTextSchool = (EditText) findViewById(R.id.editText_school);
-        editTextTShirtSize = (EditText) findViewById(R.id.editText_tshirtsize);
+
+        spinnerEditTShirtSize = (Spinner) findViewById(R.id.spinnerEditTShirtSize);
+        ArrayAdapter<CharSequence> adapterEditTShirtSize = ArrayAdapter.createFromResource(this,
+                R.array.tshirt_array, android.R.layout.simple_spinner_dropdown_item);
+        adapterEditTShirtSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEditTShirtSize.setAdapter(adapterEditTShirtSize);
+        spinnerEditTShirtSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                tShirtSizeSelected = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         editTextTechnology = (EditText) findViewById(R.id.editText_technologies);
         editTextOrginization = (EditText) findViewById(R.id.editText_orginization);
-        editTextSemester = (EditText) findViewById(R.id.editText_semester);
+
+
+        spinnerEditSemester = (Spinner) findViewById(R.id.spinnerEditSemester);
+        ArrayAdapter<CharSequence> adapterEditSemester = ArrayAdapter.createFromResource(this,
+                R.array.semester_array, android.R.layout.simple_spinner_dropdown_item);
+        adapterEditSemester.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEditSemester.setAdapter(adapterEditSemester);
+        spinnerEditSemester.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                semesterSelected = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
         Button buttonCreateUser = (Button) findViewById(R.id.btn_update_Student);
-
-        Intent intent  = getIntent();
-        String name = intent.getStringExtra(StudentActivity.EXTRA_NAME);
-        String email = intent.getStringExtra(StudentActivity.EXTRA_EMAIL);
-        String school = intent.getStringExtra(StudentActivity.EXTRA_SCHOOL);
-        String tshirtSize = intent.getStringExtra(StudentActivity.EXTRA_TSHIRTSIZE);
-        String technology = intent.getStringExtra(StudentActivity.EXTRA_TECHNOLOGY);
-        String orginiaztion = intent.getStringExtra(StudentActivity.EXTRA_ORGINIAZTION);
-        String semester = intent.getStringExtra(StudentActivity.EXTRA_SEMESTER);
-        key = intent.getStringExtra(StudentActivity.EXTRA_KEY);
-
-
-        editTextName.setText(name);
-        editTextEmail.setText(email);
-        editTextSchool.setText(school);
-        editTextTShirtSize.setText(tshirtSize);
-        editTextTechnology.setText(technology);
-        editTextOrginization.setText(orginiaztion);
-        editTextSemester.setText(semester);
-
         buttonCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateUser();
             }
         });
+
+
+        Intent intent  = getIntent();
+        String name = intent.getStringExtra(StudentActivity.EXTRA_NAME);
+        String email = intent.getStringExtra(StudentActivity.EXTRA_EMAIL);
+        String school = intent.getStringExtra(StudentActivity.EXTRA_SCHOOL);
+        tshirtSize = intent.getStringExtra(StudentActivity.EXTRA_TSHIRTSIZE);
+        String technology = intent.getStringExtra(StudentActivity.EXTRA_TECHNOLOGY);
+        String orginiaztion = intent.getStringExtra(StudentActivity.EXTRA_ORGINIAZTION);
+        semester = intent.getStringExtra(StudentActivity.EXTRA_SEMESTER);
+        key = intent.getStringExtra(StudentActivity.EXTRA_KEY);
+
+
+        editTextName.setText(name);
+        editTextEmail.setText(email);
+        editTextSchool.setText(school);
+
+        // Set Current TShirt Size Spinner
+
+        // Get array from R.array.tshirt_array
+
+
+
+
+
+        editTextTechnology.setText(technology);
+        editTextOrginization.setText(orginiaztion);
+
+        // Set Current Semester Spinner
+        // Get array from R.array.tshirt_array
+
+
+
+    }
+
+    String tshirtSize;
+    String semester;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: Get Both Spinners show preselection
+
+        for (int j = 0;  j < semesterArray.length; j++){
+            if (semesterArray[j].equals(semester)){
+                spinnerEditSemester.setSelection(j);
+                return;
+            }
+        }
+
+        for (int i = 0;  i < tShirtArray.length; i++){
+            if (tShirtArray[i].equals(tshirtSize)){
+                spinnerEditTShirtSize.setSelection(i);
+                return;
+            }
+        }
+
     }
 
     private void updateUser() {
@@ -69,13 +162,43 @@ public class UpdateStudentActivity extends AppCompatActivity {
         String name = editTextName.getText().toString();
         String email = editTextEmail.getText().toString();
         String school = editTextSchool.getText().toString();
-        String tshirtSize = editTextTShirtSize.getText().toString();
         String technology = editTextTechnology.getText().toString();
         String orginiaztion = editTextOrginization.getText().toString();
-        String semester = editTextSemester.getText().toString();
+
+        // name
+        if (TextUtils.isEmpty(name)){
+            editTextName.setError("Enter full name!");
+            return;
+        }
+        if (name.length() >= 50){
+            editTextName.setError("Name field too long!");
+        }
+
+        if (!name.matches("[a-zA-Z ]+")){
+            editTextName.setError("Enter a valid name");
+        }
+        // email
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Enter email address!");
+            return;
+        }
+
+        if (!email.contains("@") || !email.contains(".")){
+            editTextEmail.setError("Please enter a valid email!");
+            return;
+        }
+        // School
 
 
-        Student student = new Student(name,email,school,tshirtSize,technology,orginiaztion,semester,key);
+
+        // Enter Technologies
+        if (!technology.matches("[a-zA-Z ]+")){
+            editTextTechnology.setError("Enter a valid name");
+        }
+        // Enter Organization
+
+
+        Student student = new Student(name,email,school,tShirtSizeSelected,technology,orginiaztion,semesterSelected,key);
         //Update student to firebase
         ref.child(key).setValue(student);
         finish();
